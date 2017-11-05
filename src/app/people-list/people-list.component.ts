5c4134064b3edf9f0c30393dc0cd1ab01d4d5c8a
unknown <toussi69@gmail.com>
2017-11-05 14:25:44 -0800

    changed people list to use the async pipe so that it loads each person as they arrive rather than waiting for the entire observable to be ready. This includes taking out the error message and loading message for simplicity

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -9,34 +9,24 @@ import { Person }        from '../person';
 @Component({
   selector: 'app-people-list',
   template: `
-    <section *ngIf="isLoading && !errorMessage">
-      Loading our hyperdrives! Retrieving data...
-    </section>
     <ul>
-      <li *ngFor="let person of people">
+      <li *ngFor="let person of people | async">
         <a href='#' [routerLink]="['/persons', person.id]">{{person.name}}</a>
       </li>
     </ul>
-    <section *ngIf="errorMessage">
-      {{errorMessage}}
-    </section>
   `,
   styleUrls: ['./people-list.component.scss']
 })
 
 export class PeopleListComponent implements OnInit {
 
-  people:     Person[] = [];
+  people: Observable<Person[]>;
   errorMessage: string = '';
   isLoading:   boolean = true;
 
   constructor(private _peopleService: PeopleService) { }
 
   ngOnInit() { 
-    this._peopleService
-        .getAll() //second arg of the subscribe method handles errors. 
-        .subscribe(p => this.people       = p,
-                   e => this.errorMessage = e, //second one is error handling path. 
-                  () => this.isLoading    = false); 
+    this.people = this._peopleService.getAll(); 
   }
 }
